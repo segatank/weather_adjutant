eb1329517a62f027d23afd251fd218705abf818c
Maksym_Andrieiev <in.hate.i.trust@gmail.com>
2018-09-21 15:14:40 +0300

    added action for fetching data from api

--- src/actions/ActionTypes.js
+++ src/actions/ActionTypes.js
@@ -1,10 +1,25 @@
 
+const API_KEY = "eaa83a5cca70c54b19c91e237911560b";   //unique key from account registration on openweathermap
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
 export const FETCH_WEATHER = "FETCH_WEATHER";
 
-export function fetchWeather(city) {
+export function fetchWeather(city='London') {
+  const url = `${ROOT_URL}&q=${city},us`;
+  let xhr = new XMLHttpRequest();
 
-  return {
-    type: FETCH_WEATHER,
-    payload: 'some_result'
-  };
+  xhr.open('GET', url, false);
+  xhr.send();
+
+  let request = '';
+  if (xhr.status != 200) {
+    alert( xhr.status + ': ' + xhr.statusText );
+  } else {
+    request = xhr.responseText;
+    return {
+      type: FETCH_WEATHER,
+      payload: request
+    };
+  }
+
 }
