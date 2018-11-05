0873aefd5dc2c915a21e6f374f6378bb793f8372
Maksym_Andrieiev <in.hate.i.trust@gmail.com>
2018-11-05 10:43:02 +0200

    mostly finished setuping webpack for development mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const webpack = require('webpack');
 const path = require('path');
 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const APP_DIR = path.resolve(__dirname, './src');
+const BUILD_DIR = path.resolve(__dirname, 'public');
+
+//  entry: "./src/index.js",  //old entry
 module.exports = {
-  entry: "./src/index.js",
+  entry: ['@babel/polyfill', APP_DIR],
   output: {
       filename: 'output.js',
-      path: path.resolve(__dirname, 'public')
+      path: BUILD_DIR
   },
   module: {
     rules: [
@@ -23,6 +31,10 @@ module.exports = {
     ]
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './build/index.html'
+    }),
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css"
